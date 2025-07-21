f79a9432562c5b4dd0ced97ed6da74ac0934bc9f
Kev-M1 <kevinma321@gmail.com>
2025-07-21 14:20:38 -0500

    fin repaso nivel basico-intermedio

--- 6.adivinaElNumeroFullAluraOracle/app.js
+++ 6.adivinaElNumeroFullAluraOracle/app.js
@@ -1,50 +1,57 @@
+let secretNumber = null;
+let dinamicText = document.querySelector('#textoDinamico');
+let inputNumber = document.querySelector('#numeroIngresado');
+let tryButton = document.querySelector('#intentar');
+let restartButton = document.querySelector('#reiniciar');
+let attemptAccumulator;
 
-let dynamicTextNode = document.getElementById('textoDinamico');
-let numberInputNode = document.getElementById('numeroIngresado');
-let tryButtonNode = document.getElementById('intentar');
-let restartButtonNode = document.getElementById('reiniciar');
-let secretNumber;
-let userNumber;
-let i;
 
-function initialConditions() {
-    i = 0;
-    secretNumber = parseInt((Math.random() * 100) + 1);
-    dynamicTextNode.textContent = `Ingrese un número entero entre 1 y 100`;
-    numberInputNode.value = null;
-    restartButtonNode.setAttribute('disabled', true);
-    tryButtonNode.removeAttribute('disabled');
-    numberInputNode.removeAttribute('disabled');
-    console.log(secretNumber);
-}
 
-//Try-Button Event
-tryButtonNode.addEventListener('click', () => {
-    i += 1;
-    userNumber = parseInt(numberInputNode.value);
-    if (userNumber > 100 || userNumber <= 0) {
-        dynamicTextNode.textContent = `ERROR!! Ingrese un número entero dentro del rango establecido (de 1 a 100) (Intentos: ${i})`;
-    } else if (userNumber === secretNumber) {
-        dynamicTextNode.textContent = ` Felicidades!! Adivinaste el número secreto (${secretNumber}) (Intentos: ${i})`;
-        tryButtonNode.setAttribute('disabled', true);
-        restartButtonNode.removeAttribute('disabled');
-        numberInputNode.setAttribute('disabled', true);
-    } else if (userNumber > secretNumber) {
-        dynamicTextNode.textContent = ` Fallaste!! El número secreto es MENOR (Intentos: ${i})`;
-    } else if (userNumber < secretNumber) {
-        dynamicTextNode.textContent = ` Fallaste!! El número secreto es MAYOR (Intentos: ${i})`;
+const generateSecretNumber = () => {
+  return Math.floor(Math.random() * 101);
+};
+
+const initialConditions = () => {
+  secretNumber = generateSecretNumber();
+  dinamicText.textContent = `Ingrese un número entero entre 0 y 100`;
+  inputNumber.value = ``;
+  inputNumber.focus();
+  attemptAccumulator = 0;
+};
+
+//Events
+tryButton.addEventListener('click', (event) => {
+  let enteredNumber = parseInt(inputNumber.value);
+  inputNumber.value = ``;
+  inputNumber.focus();
+
+  if (enteredNumber < 0 || enteredNumber > 100 || isNaN(enteredNumber)) {
+    dinamicText.textContent = `ERROR!, Ingrese un valor válido (Número entero entre 0 y 100)`;
+  } else {
+    attemptAccumulator += 1;
+
+    if (enteredNumber > secretNumber) {
+      dinamicText.textContent = `El número secreto es MENOR (Intentos: ${attemptAccumulator})`;
+    } else if (enteredNumber < secretNumber) {
+      dinamicText.textContent = `El número secreto es MAYOR (Intentos: ${attemptAccumulator})`;
+    } else if (enteredNumber === secretNumber) {
+      dinamicText.textContent = `Felicidades!! Adivinaste el número secreto! (${secretNumber}) (Intentos: ${attemptAccumulator})`;
+      restartButton.removeAttribute('disabled');
+      tryButton.setAttribute('disabled', true);
     }
-    numberInputNode.value = null;
+  }
 })
 
-//Restart-Button Event
-restartButtonNode.addEventListener('click', () => {
-    initialConditions();
+restartButton.addEventListener('click', (event) => {
+  restartButton.setAttribute('disabled', true);
+  tryButton.removeAttribute('disabled');
+  initialConditions();
 })
 
 
 
-function main() {
-    initialConditions();
-}
+const main = () => {
+  initialConditions();
+  // console.log(secretNumber);
+};
 main();
